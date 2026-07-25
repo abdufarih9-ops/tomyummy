16212a2ef2dd7e39ea97da33f9f71aaf6169c7b2
abdufarih9-ops <abdufarih9@gmail.com>
2026-07-25 13:58:24 +0700

    Add index.html to cached assets in service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 const CACHE_NAME = 'kedai-cache-v2';
 const ASSETS_TO_CACHE = [
     './',
+    './index.html',
     './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js'
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js',
+    'https://unpkg.com/dexie/dist/dexie.js'
 ];
 
 // Install Service Worker dan simpan file ke cache
